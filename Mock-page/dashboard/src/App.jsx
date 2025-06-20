e799b0bddc5641b478b112f799f95cae70b2f30d
Colby <maquiret@gmail.com>
2025-06-20 16:46:18 -0500

    started second project

--- Mock-page/dashboard/src/App.jsx
+++ Mock-page/dashboard/src/App.jsx
@@ -1,38 +1,65 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import NavBar from './components/navbar';
-import Section from './components/section'
+import Section from './components/section';
 import SixCards from './components/six-cards';
-import Footer from './components/footer'
+import Footer from './components/footer';
+
 function App() {
+  const [apiData, setApiData] = useState(null);
+  const [count, setCount] = useState(0);
+
+  async function getApiData() {
+    const response = await fetch("url")
+    const data = await res.json();
+
+    setApiData(data);
+  }
+
+
+  useEffect(() => {
+    generateRandomNumber();
+  }, [count]);
 
   return (
-    <div className="w-full flex flex-col gap-3 bg-white">
-      <NavBar />
-      <Section 
-        imageLink={"https://www.apple.com/v/home/cf/images/heroes/iphone-family/hero_iphone_family__fuz5j2v5xx6y_mediumtall.jpg"}
-        sectionTitle={"iPHONE"} 
-        secondHeader={"Meet the iPhone 16 Family."}
-        buttons={["Learn More", "Shop iPhone"]}
-        subText={"Built for Apple Intelligence"}
-      />
-      <Section 
-        imageLink={"https://www.apple.com/v/home/cf/images/heroes/iphone-tradein-event/hero_iphone_tradein__fo4u4bki88ae_mediumtall.jpg"}
-        sectionTitle={"TRADE IN"} 
-        secondHeader={"Get extra credit towards a new iPhone when you trade in."}
-        buttons={["get your estimate"]}
-        subtext={"Now through 6.18"}
-      />
-      <Section 
-        imageLink={"https://www.apple.com/v/home/cf/images/heroes/macbook-air/hero_macbook_air_avail__fpm99qgohx2e_mediumtall.jpg"}
-        sectionTitle={"MacBook Air"} 
-        secondHeader={"Sky blue color. Sky high performance with M4"}
-        buttons={["Learn More", "Buy"]}
-        subText={"Built for Apple Intelligence"}
-      />
-      <SixCards />
-      <Footer />
+    <div className="w-full flex flex-col gap-3 bg-white text-black">
+      {data.name}
     </div>
   )
 }
 
 export default App
+
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   // first render of page
+//   componentDidMount() {
+
+//   }
+
+//   // did something change in state, if so re render/ update the page
+//   componentDidUpdate() {
+
+//   }
+
+//   // when we leave the page do some clean up work
+//   componentWillUnmount() {
+
+//   }
+
+//   render() {
+
+//     return (
+//       <div>stuff in here</div>
+//     )
+//   }
+// }
+
+
+
+
